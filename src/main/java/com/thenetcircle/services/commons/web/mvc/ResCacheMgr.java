05e9140e73eb5741e583fb410d47a25bf4abae8c
fan <fan@thenetcircle.com>
2015-01-09 19:07:38 +0800

    fix cache

--- src/main/java/com/thenetcircle/services/commons/web/mvc/ResCacheMgr.java
+++ src/main/java/com/thenetcircle/services/commons/web/mvc/ResCacheMgr.java
@@ -1,6 +1,9 @@
 package com.thenetcircle.services.commons.web.mvc;
 
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -12,11 +15,13 @@ import org.apache.commons.logging.LogFactory;
 import com.google.common.collect.MapMaker;
 
 @WebListener
-public class ResCacheMgr implements ServletContextListener {
+public class ResCacheMgr implements ServletContextListener, Runnable {
 
 	public static final Map<String, Object> cacheMap = new MapMaker().softValues().makeMap();
 
 	private static Log log = LogFactory.getLog(ResCacheMgr.class);
+	
+	private ScheduledExecutorService worker = null;
 
 	@Override
 	public void contextDestroyed(ServletContextEvent ce) {
@@ -27,6 +32,16 @@ public class ResCacheMgr implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent ce) {
 		String realPath = ce.getServletContext().getRealPath(".");
 		log.info("getRealPath(): " + realPath);
+		
+		log.info("start a thread to validate the cache entry");
+		
+		worker = Executors.newScheduledThreadPool(1);
+		
+		worker.scheduleAtFixedRate(this, 10, 30, TimeUnit.SECONDS);
 	}
 
+	public void run() {
+		//valid the cache entry
+		log.info("validate the cache entry");
+	}
 }
