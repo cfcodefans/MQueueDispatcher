9fa4e39f013df8cbf39710e60b2058fb969d04e8
cfcodefans <cf.chenfan@gmail.com>
2015-10-29 15:58:15 +0800

    clean up

--- src/test/java/pref_sqrt.py
+++
@@ -1,9 +0,0 @@
-from java.lang import *
-
-now = float(str(System.currentTimeMillis()))
-result = 0.0
-for i in range(1, 1000000):
-	result += Math.sqrt(i)
-	
-then = float(str(System.currentTimeMillis()))
-print("after " + str(then - now) + " ms " + str(result))
