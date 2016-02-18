d1506f818a9de32a1f9831e91673141156cfdf48
cfcodefans <cf.chenfan@gmail.com>
2016-02-18 21:13:09 +0800

    optimization

--- src/main/java/com/thenetcircle/services/dispatcher/failsafe/sql/FailedMessageSqlStorage.java
+++ src/main/java/com/thenetcircle/services/dispatcher/failsafe/sql/FailedMessageSqlStorage.java
@@ -13,47 +13,50 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.thenetcircle.services.commons.MiscUtils;
+import com.thenetcircle.services.commons.actor.AsyncActor;
 import com.thenetcircle.services.commons.actor.ConcurrentAsynActor;
 import com.thenetcircle.services.commons.persistence.jpa.BaseDao;
 import com.thenetcircle.services.commons.persistence.jpa.JpaModule;
 import com.thenetcircle.services.dispatcher.entity.MessageContext;
-import com.thenetcircle.services.dispatcher.entity.MsgResp;
 import com.thenetcircle.services.dispatcher.failsafe.IFailsafe;
 import com.thenetcircle.services.dispatcher.http.HttpDispatcherActor;
 
-public class FailedMessageSqlStorage  extends ConcurrentAsynActor<MessageContext> implements IFailsafe {
+public class FailedMessageSqlStorage extends ConcurrentAsynActor<MessageContext> implements AsyncActor.IBatchProvider<MessageContext>, IFailsafe {
 
-	private static FailedMessageSqlStorage instance = new FailedMessageSqlStorage();
-	protected static final Log log = LogFactory.getLog(FailedMessageSqlStorage.class.getSimpleName());
+	private static FailedMessageSqlStorage	instance	= new FailedMessageSqlStorage();
+	protected static final Log				log			= LogFactory.getLog(FailedMessageSqlStorage.class.getSimpleName());
 
 	public static FailedMessageSqlStorage instance() {
 		return instance;
 	}
-	
-	private EntityManager em = null;
-	
-	final ExecutorService executor = Executors.newSingleThreadExecutor(MiscUtils.namedThreadFactory(FailedMessageSqlStorage.class.getSimpleName()));
+
+	private EntityManager	em			= null;
+
+	final ExecutorService	executor	= Executors.newSingleThreadExecutor(MiscUtils.namedThreadFactory(FailedMessageSqlStorage.class.getSimpleName()));
 
 	private FailedMessageSqlStorage() {
 		start();
 	}
-	
+
 	public void delete(final Long id) {
 		final Query q = em.createQuery("delete from MessageContext mc where mc.id=:id");
 		q.setParameter("id", id);
 		q.executeUpdate();
 	}
 
-	public MessageContext handle(final MessageContext mc) {
-		if (mc == null) return mc;
-		
+	public void handle(final Collection<MessageContext> mcs) {
+		if (CollectionUtils.isEmpty(mcs))
+			return;
+
 		try {
 			em = JpaModule.getEntityManager();
-			
+
 			EntityTransaction transaction = null;
 			try {
 				transaction = BaseDao.beginTransaction(em);
-				_handle(mc);
+				for (MessageContext mc : mcs) {
+					_handle(mc);
+				}
 				transaction.commit();
 				em.close();
 			} catch (Exception e) {
@@ -61,7 +64,7 @@ public class FailedMessageSqlStorage  extends ConcurrentAsynActor<MessageContext
 				if (transaction != null && transaction.isActive())
 					transaction.setRollbackOnly();
 				BaseDao.endTransaction(em);
-				
+
 				log.error("close EntityManager");
 				em.close();
 			}
@@ -69,22 +72,23 @@ public class FailedMessageSqlStorage  extends ConcurrentAsynActor<MessageContext
 			if (e instanceof InterruptedException) {
 				throw e;
 			}
-			
+
 			log.error("failed by exception", e);
 		}
-		return mc;
+		return;
 	}
 
 	public MessageContext _handle(final MessageContext mc) {
-		if (mc == null) return mc;
+		if (mc == null)
+			return mc;
 		try {
 			if (mc.isSucceeded() && mc.getId() > 0) {
 				delete(mc.getId());
 				return mc;
 			}
-			
+
 			mc.fail();
-			
+
 			MessageContext _mc = em.find(MessageContext.class, Long.valueOf(mc.getId()));
 			if (_mc == null) {
 				_mc = em.merge(mc);
@@ -95,7 +99,7 @@ public class FailedMessageSqlStorage  extends ConcurrentAsynActor<MessageContext
 				_mc.setResponse(mc.getResponse());
 				em.merge(_mc);
 			}
-			
+
 			return HttpDispatcherActor.instance().handover(mc);
 		} catch (Exception e) {
 			log.error("failed to handle: \n\t" + mc, e);
@@ -123,5 +127,10 @@ public class FailedMessageSqlStorage  extends ConcurrentAsynActor<MessageContext
 		if (em != null && em.isOpen()) {
 			em.close();
 		}
+	}
+
+	@Override
+	public Collection<MessageContext> pollBatch() throws Exception {
+		return super.pollBatch(10);
 	}
 }
