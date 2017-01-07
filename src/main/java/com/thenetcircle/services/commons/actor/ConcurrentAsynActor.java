c0355b9d62004132de894dda2dd405e697b72a8d
cfcodefans <cf.chenfan@gmail.com>
2017-01-07 11:08:33 +0800

    ..

--- src/main/java/com/thenetcircle/services/commons/actor/ConcurrentAsynActor.java
+++ src/main/java/com/thenetcircle/services/commons/actor/ConcurrentAsynActor.java
@@ -1,28 +1,29 @@
 package com.thenetcircle.services.commons.actor;
 
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class ConcurrentAsynActor<M> extends AsyncActor<M, ConcurrentLinkedDeque<M>> {
+public class ConcurrentAsynActor<M> extends AsyncActor<M, ConcurrentLinkedQueue<M>> {
 
-	public ConcurrentAsynActor(ConcurrentLinkedDeque<M> _buf) {
-		super(_buf);
-	}
+    public ConcurrentAsynActor(ConcurrentLinkedQueue<M> _buf) {
+        super(_buf);
+    }
 
-	public ConcurrentAsynActor() {
-		this(new ConcurrentLinkedDeque<M>());
-	}
+    public ConcurrentAsynActor() {
+        this(new ConcurrentLinkedQueue<M>());
+    }
 
-	protected M poll(ConcurrentLinkedDeque<M> _buf) throws InterruptedException {
-		M polled = _buf.poll();
-		if (polled != null) {
-			return polled;
-		}
+    protected M poll(ConcurrentLinkedDeque<M> _buf) throws InterruptedException {
+        M polled = _buf.poll();
+        if (polled != null) {
+            return polled;
+        }
 
-		Thread.sleep(1);
-		return null;
-	}
-	
-	protected M poll() throws InterruptedException {
-		return poll(super.buf);
-	}
+        Thread.sleep(1);
+        return null;
+    }
+
+    protected M poll() throws InterruptedException {
+        return poll(super.buf);
+    }
 }
